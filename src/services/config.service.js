1fa82fa1dbf1c20ef0bd32eff00aa932efc67d9e
yanky <alvarezyanky7@gmail.com>
2025-02-16 01:01:09 +0100

    updated railway host

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -1,5 +1,5 @@
 // const API_URL = "http://localhost:3000";
-const API_URL = "lach-karaoke-server.railway.internal";
+const API_URL = "lach-karaoke-server-production.up.railway.app";
 
 // const TEST_HOST = "http://localhost:5173";
 const TEST_HOST = "https://lach-karaoke.netlify.app";
