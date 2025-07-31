97b62dd365d250fc904f9458efcbf7eaccd23a75
yanky <alvarezyanky7@gmail.com>
2025-07-31 21:36:53 +0200

    changed host

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,6 +2,6 @@
 const API_URL = "https://lach-karaoke-server-production.up.railway.app";
 
 // const TEST_HOST = "http://localhost:5173";
-const TEST_HOST = "https://lach-karaoke.netlify.app";
+const TEST_HOST = "https://galipote.netlify.app";
 
 export { API_URL, TEST_HOST };
